46473c007169bdbc88f780bec16632ba6868ad51
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-11-15 19:02:58 -0500

    Complete step 5

--- script.js
+++ script.js
@@ -11,10 +11,9 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
     let choice = prompt("your choice:");
+    choice = choice.toLowerCase();
     switch (choice) {
         case "rock":
             return "rock";
@@ -31,7 +30,46 @@ function getHumanChoice() {
     }
 }
 
-console.log(getHumanChoice());
-
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    console.log(computerChoice);
+    console.log(humanChoice);
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            computerScore++;
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+        } else if (computerChoice === "scissors") {
+            humanScore++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+        } else {
+            console.log(`You can't put ${humanChoice} against ${computerChoice}, try again!`)
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            computerScore++;
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+        } else if (computerChoice === "rock") {
+            humanScore++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+        } else {
+            console.log(`You can't put ${humanChoice} against ${computerChoice}, try again!`)
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            computerScore++;
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+        } else if (computerChoice === "paper") {
+            humanScore++;
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+        } else {
+            console.log(`You can't put ${humanChoice} against ${computerChoice}, try again!`)
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
