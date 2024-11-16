51f428696b74622dbb253df53cac1e52e71c0064
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-11-15 20:26:24 -0500

    Complete most of step 6

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-console.log("Hello World");
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     let num = Math.floor((Math.random() * 3) + 1);
@@ -22,44 +23,67 @@ function getHumanChoice() {
     }
 }
 
+function youWin(humanChoice, computerChoice) {
+    humanScore++;
+    console.log(`you win! ${humanChoice} beats ${computerChoice}`);
+}
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function youLose(humanChoice, computerChoice) {
+    computerScore++;
+    console.log(`you lose... ${computerChoice} beats ${humanChoice}`)
+}
 
-function playRound(humanChoice, computerChoice) {
-    let humanScore = 0;
-    let computerScore = 0;
-    if (humanChoice === "rock") {
-        if (computerChoice === "paper") {
-            computerScore++;
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
-        } else if (computerChoice === "scissors") {
-            humanScore++;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+function youTie(humanChoice, computerChoice) {
+    console.log(`You can't put ${humanChoice} against ${computerChoice}, try again!`)
+}
+
+function playRound() {
+
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    if (humanSelection === "rock") {
+        if (computerSelection === "paper") {
+            youLose(humanSelection, computerSelection);
+        } else if (computerSelection === "scissors") {
+            youWin(humanSelection, computerSelection);
         } else {
-            console.log(`You can't put ${humanChoice} against ${computerChoice}, try again!`)
+            youTie(humanSelection, computerSelection);
         }
-    } else if (humanChoice === "paper") {
-        if (computerChoice === "scissors") {
-            computerScore++;
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
-        } else if (computerChoice === "rock") {
-            humanScore++;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+
+    } else if (humanSelection === "paper") {
+        if (computerSelection === "scissors") {
+            youLose(humanSelection, computerSelection);
+        } else if (computerSelection === "rock") {
+            youWin(humanSelection, computerSelection);
         } else {
-            console.log(`You can't put ${humanChoice} against ${computerChoice}, try again!`)
+            youTie(humanSelection, computerSelection);
         }
-    } else if (humanChoice === "scissors") {
-        if (computerChoice === "rock") {
-            computerScore++;
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
-        } else if (computerChoice === "paper") {
-            humanScore++;
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+
+    } else if (humanSelection === "scissors") {
+        if (computerSelection === "rock") {
+            youLose(humanSelection, computerSelection);
+        } else if (computerSelection === "paper") {
+            youWin(humanSelection, computerSelection);
         } else {
-            console.log(`You can't put ${humanChoice} against ${computerChoice}, try again!`)
+            youTie(humanSelection, computerSelection);
         }
+    }
+    console.log(`human ${humanSelection} computer ${computerSelection}`)
+    console.log(`human ${humanScore} computer ${computerScore}`);
+}
+
+function playGame () {
+    for (i = 0; i < 5; i++){
+        playRound();
+    };
+    if (humanScore > computerScore) {
+        console.log(`you won ${humanScore} to ${computerScore}`);
+    } else if (humanScore < computerScore) {
+        console.log(`you lost ${humanScore} to ${computerScore}`);
+    } else {
+        console.log(`you tied... so nobody wins`);
     }
 }
 
-playRound();
+playGame();
